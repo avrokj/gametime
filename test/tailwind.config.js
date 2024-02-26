70e74fa70977661a7f2e4e3af42f665439ba4c88
avrokj <jan.korva@gmail.com>
2024-02-27 00:37:26 +0200

    animations added

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     content: ["./*.{html,js}"],
     darkMode: "selector",
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                typing: {
+                    "0%": {
+                        width: "0%",
+                        visibility: "hidden",
+                    },
+                    "100%": {
+                        width: "100%",
+                    },
+                },
+                blink: {
+                    "50%": {
+                        borderColor: "transparent",
+                    },
+                    "100%": {
+                        borderColor: "white",
+                    },
+                },
+            },
+            animation: {
+                typing: "typing 4s steps(28) infinite alternate, blink .7s infinite",
+            },
+        },
     },
     plugins: [],
 };
