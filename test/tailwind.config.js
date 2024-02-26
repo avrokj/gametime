849378b0ae82c76384e77ec702ad96778c9fcf5a
avrokj <jan.korva@gmail.com>
2024-02-27 00:10:23 +0200

    test added tailwind

---
+++ test/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    darkMode: "selector",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
