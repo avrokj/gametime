69249c70e11a0205c0809b56eef512641ee05b64
avrokj <jan.korva@gmail.com>
2024-02-28 00:18:13 +0200

    7-segment tailwind, added away and +2, +3

--- test/7-segment/app.js
+++ test/7-segment/app.js
@@ -1,39 +1,117 @@
-import { GameBoard } from "./src/GameBoard.js";
+import { HomeGameBoard, AwayGameBoard } from "./src/GameBoard.js";
 import { Number } from "./src/Number.js";
 
 const boardSizeX = 7;
 const boardSizeY = 5;
 
 const number = new Number();
-const gameBoard = new GameBoard(boardSizeX, boardSizeY);
+const homeGameBoard = new HomeGameBoard(boardSizeX, boardSizeY);
+const awayGameBoard = new AwayGameBoard(boardSizeX, boardSizeY);
 
-gameBoard.draw(number.getCoordinates(0));
+homeGameBoard.draw(number.getCoordinates(0));
+awayGameBoard.draw(number.getCoordinates(0));
 
 const homeMinus1 = document.getElementById("homeMinus1");
 const homePlus1 = document.getElementById("homePlus1");
+const homePlus2 = document.getElementById("homePlus2");
+const homePlus3 = document.getElementById("homePlus3");
 const homeScore = document.getElementById("homeScore");
 
+const awayMinus1 = document.getElementById("awayMinus1");
+const awayPlus1 = document.getElementById("awayPlus1");
+const awayPlus2 = document.getElementById("awayPlus2");
+const awayPlus3 = document.getElementById("awayPlus3");
+const awayScore = document.getElementById("awayScore");
+
 var hScore = 0;
+var aScore = 0;
 homeScore.innerHTML = hScore;
+awayScore.innerHTML = aScore;
 
-const handleHomeScorePlus = () => {
-    hScore++;
-    if (hScore > 99){
-        hScore = 0;
-    }
-    homeScore.innerHTML = hScore;
-    gameBoard.draw(number.getCoordinates(hScore));
-    console.log(number.getCoordinates(hScore))
-  };
-
-  const handleHomeScoreMinus = () => {
+const handleHomeScoreMinus = () => {
     hScore--;
-    if (hScore < 0){
+    if (hScore < 0) {
         hScore = 99;
     }
     homeScore.innerHTML = hScore;
-    gameBoard.draw(number.getCoordinates(hScore));
-  };
+    homeGameBoard.draw(number.getCoordinates(hScore));
+};
 
-  homePlus1.addEventListener("click", handleHomeScorePlus);
-  homeMinus1.addEventListener("click", handleHomeScoreMinus);
+const handleAwayScoreMinus = () => {
+    aScore--;
+    if (aScore < 0) {
+        aScore = 99;
+    }
+    awayScore.innerHTML = aScore;
+    awayGameBoard.draw(number.getCoordinates(aScore));
+};
+
+const handleHomeScorePlus = () => {
+    hScore++;
+    if (hScore > 99) {
+        hScore = 0;
+    }
+    homeScore.innerHTML = hScore;
+    homeGameBoard.draw(number.getCoordinates(hScore));
+    console.log(number.getCoordinates(hScore));
+};
+
+const handleAwayScorePlus = () => {
+    aScore++;
+    if (aScore > 99) {
+        aScore = 0;
+    }
+    awayScore.innerHTML = aScore;
+    awayGameBoard.draw(number.getCoordinates(aScore));
+    console.log(number.getCoordinates(aScore));
+};
+
+const handleHomeScorePlus2 = () => {
+    hScore += 2;
+    if (hScore > 99) {
+        hScore = 0;
+    }
+    homeScore.innerHTML = hScore;
+    homeGameBoard.draw(number.getCoordinates(hScore));
+    console.log(number.getCoordinates(hScore));
+};
+
+const handleAwayScorePlus2 = () => {
+    aScore += 2;
+    if (aScore > 99) {
+        aScore = 0;
+    }
+    awayScore.innerHTML = aScore;
+    awayGameBoard.draw(number.getCoordinates(aScore));
+    console.log(number.getCoordinates(aScore));
+};
+
+const handleHomeScorePlus3 = () => {
+    hScore += 3;
+    if (hScore > 99) {
+        hScore = 0;
+    }
+    homeScore.innerHTML = hScore;
+    homeGameBoard.draw(number.getCoordinates(hScore));
+    console.log(number.getCoordinates(hScore));
+};
+
+const handleAwayScorePlus3 = () => {
+    aScore += 3; // Increment aScore by 3
+    if (aScore > 99) {
+        aScore = 0;
+    }
+    awayScore.innerHTML = aScore;
+    awayGameBoard.draw(number.getCoordinates(aScore));
+    console.log(number.getCoordinates(aScore));
+};
+
+homeMinus1.addEventListener("click", handleHomeScoreMinus);
+homePlus1.addEventListener("click", handleHomeScorePlus);
+homePlus2.addEventListener("click", handleHomeScorePlus2);
+homePlus3.addEventListener("click", handleHomeScorePlus3);
+
+awayMinus1.addEventListener("click", handleAwayScoreMinus);
+awayPlus1.addEventListener("click", handleAwayScorePlus);
+awayPlus2.addEventListener("click", handleAwayScorePlus2);
+awayPlus3.addEventListener("click", handleAwayScorePlus3);
--- test/7-segment/src/GameBoard.js
+++ test/7-segment/src/GameBoard.js
@@ -1,62 +1,117 @@
-class GameBoard {
-
-    gameBoardTable = document.getElementById('game-board');
+class HomeGameBoard {
+    gameBoardTable = document.getElementById("home-game-board");
     boardSize;
 
-    constructor ( boardSizeX, boardSizeY) {
+    constructor(boardSizeX, boardSizeY) {
         this.boardSizeX = boardSizeX;
         this.boardSizeY = boardSizeY;
     }
-    
-    draw ( numberCoordinates ) {
 
-        this.gameBoardTable.innerHTML = '';
+    draw(numberCoordinates) {
+        this.gameBoardTable.innerHTML = "";
 
-        for ( let i=0; i < this.boardSizeY; i++) {
+        for (let i = 0; i < this.boardSizeY; i++) {
+            const rowTr = document.createElement("tr");
 
-            const rowTr = document.createElement('tr');
+            for (let j = 0; j < this.boardSizeX; j++) {
+                const cellTd = document.createElement("td");
+                const id = i + "-" + j;
+                cellTd.setAttribute("width", 30);
+                cellTd.setAttribute("height", 30);
+                cellTd.setAttribute("id", id);
 
-            for ( let j=0; j < this.boardSizeX; j++) {
-
-                const cellTd = document.createElement('td');
-                const id = i + '-' + j;
-                cellTd.setAttribute('width' , 30);
-                cellTd.setAttribute('height' , 30);
-                cellTd.setAttribute('id', id);
-                
-            
                 if (j == 1 || j == 5) {
-                    cellTd.setAttribute('width' , 120);
+                    cellTd.setAttribute("width", 120);
                 }
 
-                if (j == 3 ) {
-                    cellTd.setAttribute('width' , 60);
+                if (j == 3) {
+                    cellTd.setAttribute("width", 60);
                 }
 
-                if ( numberCoordinates.includes(id) ){
-                    cellTd.classList.add('number');
+                if (numberCoordinates.includes(id)) {
+                    cellTd.classList.add("number");
                     cellTd.style.background = "red";
                 }
 
-                if (i == 1 || i == 3){
-                    cellTd.setAttribute('height' , 120);
+                if (i == 1 || i == 3) {
+                    cellTd.setAttribute("height", 120);
                 }
 
-                if ( i % 2 == 0  && (j == 1 || j == 5)){
+                if (i % 2 == 0 && (j == 1 || j == 5)) {
                     cellTd.style.border = "1px solid pink";
+                    cellTd.style.borderRadius = "15px";
                 }
-                if ( i % 2 == 1  && j % 2 == 0){
+                if (i % 2 == 1 && j % 2 == 0) {
                     cellTd.style.border = "1px solid pink";
+                    cellTd.style.borderRadius = "15px";
                 }
-
 
                 rowTr.append(cellTd);
                 //console.log(id);
             }
-    
+
             this.gameBoardTable.append(rowTr);
         }
     }
-};
+}
 
-export { GameBoard };
+export { HomeGameBoard };
+
+class AwayGameBoard {
+    gameBoardTable = document.getElementById("away-game-board");
+    boardSize;
+
+    constructor(boardSizeX, boardSizeY) {
+        this.boardSizeX = boardSizeX;
+        this.boardSizeY = boardSizeY;
+    }
+
+    draw(numberCoordinates) {
+        this.gameBoardTable.innerHTML = "";
+
+        for (let i = 0; i < this.boardSizeY; i++) {
+            const rowTr = document.createElement("tr");
+
+            for (let j = 0; j < this.boardSizeX; j++) {
+                const cellTd = document.createElement("td");
+                const id = i + "-" + j;
+                cellTd.setAttribute("width", 30);
+                cellTd.setAttribute("height", 30);
+                cellTd.setAttribute("id", id);
+
+                if (j == 1 || j == 5) {
+                    cellTd.setAttribute("width", 120);
+                }
+
+                if (j == 3) {
+                    cellTd.setAttribute("width", 60);
+                }
+
+                if (numberCoordinates.includes(id)) {
+                    cellTd.classList.add("number");
+                    cellTd.style.background = "red";
+                }
+
+                if (i == 1 || i == 3) {
+                    cellTd.setAttribute("height", 120);
+                }
+
+                if (i % 2 == 0 && (j == 1 || j == 5)) {
+                    cellTd.style.border = "1px solid pink";
+                    cellTd.style.borderRadius = "15px";
+                }
+                if (i % 2 == 1 && j % 2 == 0) {
+                    cellTd.style.border = "1px solid pink";
+                    cellTd.style.borderRadius = "15px";
+                }
+
+                rowTr.append(cellTd);
+                //console.log(id);
+            }
+
+            this.gameBoardTable.append(rowTr);
+        }
+    }
+}
+
+export { AwayGameBoard };
