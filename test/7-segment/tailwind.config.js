022f35caaa40294373724255dc4345b6cfa3fcdc
avrokj <jan.korva@gmail.com>
2024-03-07 00:24:50 +0200

    daisy ui

--- test/7-segment/tailwind.config.js
+++ test/7-segment/tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    daisyui: {
+        themes: ["cupcake", "synthwave"],
+    },
+    plugins: [require("daisyui")],
 };
