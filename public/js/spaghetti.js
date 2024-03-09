a9516b7fa91ed4133e08f53728e500be1eed5631
avrokj <jan.korva@gmail.com>
2024-03-09 09:47:01 +0200

    added 7 segment link

--- public/js/spaghetti.js
+++ public/js/spaghetti.js
@@ -1,38 +1,41 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*******************************************!*\
+/******/ (() => {
+    // webpackBootstrap
+    var __webpack_exports__ = {};
+    /*!*******************************************!*\
   !*** ./resources/js/segment/spaghetti.js ***!
   \*******************************************/
-// Function to toggle dark mode
-function toggleDarkMode() {
-  var body = document.body;
-  body.classList.toggle("dark-mode");
-  // Store the user's preference in local storage
-  var isDarkMode = body.classList.contains("dark-mode");
-  localStorage.setItem("darkMode", isDarkMode);
-  // Toggle Daisy UI theme classes
-  if (isDarkMode) {
-    document.documentElement.dataset.theme = "synthwave";
-  } else {
-    document.documentElement.dataset.theme = "cupcake";
-  }
-}
+    // Function to toggle dark mode
+    function toggleDarkMode() {
+        var body = document.body;
+        body.classList.toggle("dark-mode");
+        // Store the user's preference in local storage
+        var isDarkMode = body.classList.contains("dark-mode");
+        localStorage.setItem("darkMode", isDarkMode);
+        // Toggle Daisy UI theme classes
+        if (isDarkMode) {
+            document.documentElement.dataset.theme = "dracula";
+        } else {
+            document.documentElement.dataset.theme = "light";
+        }
+    }
 
-// Function to initialize dark mode based on user's preference
-function initializeDarkMode() {
-  var savedDarkMode = localStorage.getItem("darkMode");
-  if (savedDarkMode === "true") {
-    document.body.classList.add("dark-mode");
-    document.documentElement.dataset.theme = "synthwave";
-  } else {
-    document.documentElement.dataset.theme = "cupcake";
-  }
-}
+    // Function to initialize dark mode based on user's preference
+    function initializeDarkMode() {
+        var savedDarkMode = localStorage.getItem("darkMode");
+        if (savedDarkMode === "true") {
+            document.body.classList.add("dark-mode");
+            document.documentElement.dataset.theme = "dracula";
+        } else {
+            document.documentElement.dataset.theme = "light";
+        }
+    }
 
-// Add event listener to dark mode toggle checkbox
-document.getElementById("darkModeCheckbox").addEventListener("change", toggleDarkMode);
+    // Add event listener to dark mode toggle checkbox
+    document
+        .getElementById("darkModeCheckbox")
+        .addEventListener("change", toggleDarkMode);
 
-// Initialize dark mode
-initializeDarkMode();
-/******/ })()
-;
+    // Initialize dark mode
+    initializeDarkMode();
+    /******/
+})();
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('segment.index')" :active="request()->routeIs('segment')">
+                        {{ __('7 Segment') }}
+                    </x-nav-link>
                 </div>
             </div>
 
