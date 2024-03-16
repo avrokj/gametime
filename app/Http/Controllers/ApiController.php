b161bca7e6746133da07fcda9fd8af11932c14a4
Maarma <Maarma25@gmail.com>
2024-03-16 10:25:31 +0200

    sb api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Api;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function getBySbId($sbId)
+    {
+        $apiData = Api::where('sb_id', $sbId)->first();
+        if ($apiData) {
+            $responseData = [
+                'last_command' => $apiData->last_command,
+                'home_score' => $apiData->home_score,
+                'away_score' => $apiData->away_score,
+            ];
+            return response()->json($responseData);
+        } else {
+            return response()->json(['error' => 'No data found for given scoreboard'], 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/apis/{sbId}', [ApiController::class, 'getBySbId']);
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
