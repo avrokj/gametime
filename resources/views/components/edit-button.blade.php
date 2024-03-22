7a3b8891eb9cf0c4f8594c3b370b82b7f20ef5f7
avrokj <jan.korva@gmail.com>
2024-03-23 01:02:12 +0200

    modal fix, added blade icons

--- resources/views/components/edit-button.blade.php
+++ resources/views/components/edit-button.blade.php
@@ -1,3 +1,3 @@
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-info btn-sm mx-1']) }}>
-    {{ $slot }}
+    <x-heroicon-s-pencil-square class="w-6 text-white" /> {{ $slot }}
 </button>
--- resources/views/sports/index.blade.php
+++ resources/views/sports/index.blade.php
@@ -12,9 +12,11 @@
           <x-primary-button type="submit">Search</x-primary-button>
         </form>
       </div>
-      <div>
-        <!-- Open the modal using ID.showModal() method -->
-        <x-primary-button onclick="my_modal_add.showModal()">{{ __('Create sport') }}</x-primary-button>
+      <div>       
+        <x-primary-button onclick="document.getElementById('my_modal_add').showModal()">
+          {{ __('Create sport') }}
+        </x-primary-button>
+
         <dialog id="my_modal_add" class="modal modal-bottom sm:modal-middle">
           <div class="modal-box">
             <h3 class="font-bold text-lg">{{ __('Add Sport') }}</h3>
@@ -26,8 +28,10 @@
                 <x-text-input name="sports_name" />
                 <x-input-error :messages="$errors->get('sports_name')" class="mt-2" />
                 <div class="mt-4 space-x-2">
-                    <x-save-button>{{ __('Save') }}</x-save-button>
-                    <a href="{{ route('sports.index') }}">{{ __('Cancel') }}</a>
+                  <x-save-button> {{ __('Save') }}</x-save-button>
+                  <x-cancel-button onclick="window.location='{{ route('sports.index') }}'">
+                    {{ __('Cancel') }}
+                  </x-cancel-button>
                 </div>
             </form>
             </div>
@@ -57,12 +61,9 @@
                 <td>
                   <div class="flex justify-end">                    
                     <!-- Open the modal using ID.showModal() method -->
-                    <x-edit-button onclick="my_modal_edit.showModal()">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                      </svg>
+                    <x-edit-button onclick="document.getElementById('my_modal_edit{{ $sport->id }}').showModal()">                      
                     </x-edit-button>
-                    <dialog id="my_modal_edit" class="modal modal-bottom sm:modal-middle">
+                    <dialog id="my_modal_edit{{ $sport->id }}" class="modal modal-bottom sm:modal-middle">
                       <div class="modal-box">
                         <h3 class="font-bold text-lg">{{ __('Edit Sport') }}</h3>
                         <!-- <p class="py-4">Press ESC key or click the button below to close</p> -->
@@ -73,8 +74,10 @@
                             <x-text-input name="sports_name" value="{{ old('sports_name', $sport->sports_name) }}" />
                             <x-input-error :messages="$errors->get('message')" class="mt-2" />
                             <div class="mt-4 space-x-2">
-                              <x-save-button>{{ __('Save') }}</x-save-button>
-                              <a href="{{ route('sports.index') }}">{{ __('Cancel') }}</a>
+                              <x-save-button> {{ __('Save') }}</x-save-button>
+                              <x-cancel-button onclick="window.location='{{ route('sports.index') }}'">
+                                {{ __('Cancel') }}
+                              </x-cancel-button>
                             </div>
                           </form>
                         </div>
@@ -84,9 +87,7 @@
                     @csrf
                     @method('delete')
                     <x-delete-button onclick="return confirm('Are you sure?'); event.preventDefault(); this.closest('form').submit();">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                      </svg>                      
+                      <x-heroicon-s-trash class="w-6 text-white" />
                     </x-delete-button>
                   </form>
                   </div>
