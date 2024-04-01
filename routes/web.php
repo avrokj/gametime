32bf326c9cbdda46b5362445c8d96b45c52e9f76
EP512 <karel.maarma@ouman.ee>
2024-04-01 11:11:03 +0300

    web.php line 88 //

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,6 @@ Route::get('/segment/player', [SegmentController::class, 'player'])->name('segme
 Route::get('/score', [ScoreController::class, 'index'])->name('score.index');
 Route::post('/score', [ScoreController::class, 'updateScore'])->name('score.index');
 
-Route::post('/apis', [ApiController::class, 'updateApi'])->name('score.index');
+//Route::post('/apis', [ApiController::class, 'updateApi'])->name('score.index');
 
 require __DIR__ . '/auth.php';
