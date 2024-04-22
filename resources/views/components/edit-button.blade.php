698b7078c2f658913b66dfd9c5d2c0891d1437e6
avrokj <jan.korva@gmail.com>
2024-04-22 16:21:33 +0300

    change to edit colors if needed

--- resources/views/components/edit-button.blade.php
+++ resources/views/components/edit-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'p-2 focus:outline-none focus:shadow-outline', 'title' => 'Edit']) }}>
-    <x-heroicon-s-pencil-square class="w-6 text-blue-500 hover:text-blue-600" /> {{ $slot }}
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'p-2 focus:outline-none focus:shadow-outline text-blue-500 hover:text-blue-600', 'title' => 'Edit']) }}>
+    <x-heroicon-s-pencil-square class="w-6" /> {{ $slot }}
 </button>
