05ca7dbabd067f9c8009193547d256882162bd3c
avrokj <jan.korva@gmail.com>
2024-05-05 23:56:43 +0300

    added accordion and tabs js

--- resources/js/spaghetti.js
+++ resources/js/spaghetti.js
@@ -31,3 +31,76 @@ document
 
 // Initialize dark mode
 initializeDarkMode();
+
+// Function to Accordion
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionTriggers = document.querySelectorAll(".accordion-trigger");
+
+    accordionTriggers.forEach((trigger) => {
+        trigger.addEventListener("click", function () {
+            const content =
+                this.parentElement.querySelector(".collapse-content");
+            const radio = this.parentElement.querySelector(
+                'input[type="radio"]'
+            );
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                radio.checked = false;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 40 + "px";
+                radio.checked = true;
+            }
+        });
+    });
+});
+
+// Function to Tabs
+document.addEventListener("DOMContentLoaded", function () {
+    const eventTabs = document.querySelectorAll(".event-tabs .tab");
+    eventTabs.forEach((tab) => {
+        tab.addEventListener("click", function (event) {
+            event.preventDefault();
+            // Hide all tab contents
+            document
+                .querySelectorAll('[role="eventtabpanel"]')
+                .forEach((tabContent) => {
+                    tabContent.classList.add("hidden");
+                });
+            // Remove active class from all tabs
+            document.querySelectorAll(".event-tabs .tab").forEach((tab) => {
+                tab.classList.remove("font-semibold");
+                tab.classList.remove("tab-active");
+            });
+            // Show the clicked tab content
+            const tabId = this.getAttribute("href").substring(1);
+            document.getElementById(tabId).classList.remove("hidden");
+            // Add active class to the clicked tab
+            this.classList.add("font-semibold");
+            this.classList.add("tab-active");
+        });
+    });
+
+    const gameTabs = document.querySelectorAll(".game-tabs .tab");
+    gameTabs.forEach((tab) => {
+        tab.addEventListener("click", function (event) {
+            event.preventDefault();
+            // Hide all tab contents
+            document
+                .querySelectorAll('[role="gametabpanel"]')
+                .forEach((tabContent) => {
+                    tabContent.classList.add("hidden");
+                });
+            // Remove active class from all tabs
+            document.querySelectorAll(".game-tabs .tab").forEach((tab) => {
+                tab.classList.remove("font-semibold");
+                tab.classList.remove("tab-active");
+            });
+            // Show the clicked tab content
+            const tabId = this.getAttribute("href").substring(1);
+            document.getElementById(tabId).classList.remove("hidden");
+            // Add active class to the clicked tab
+            this.classList.add("font-semibold");
+            this.classList.add("tab-active");
+        });
+    });
+});
