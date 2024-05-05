d6814ab64af15fa5d53b79fcf7df7e8549b1be70
avrokj <jan.korva@gmail.com>
2024-05-05 23:58:22 +0300

    welcome page update

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use App\Models\Game;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $events = Event::all();
+        $games = Game::all();
+        $teams = Team::all();
+
+        return view('welcome', [
+            'events' => $events,
+            'games' => $games,
+            'teams' => $teams
+        ]);
+    }
+}
---
+++ app/Models/Welcome.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    public function sport()
+    {
+        return $this->belongsTo(Sport::class);
+    }
+
+    public function coach()
+    {
+        return $this->belongsTo(Coach::class);
+    }
+
+    public function coaches()
+    {
+        return $this->hasMany(Coach::class, 'id');
+    }
+
+    public function players()
+    {
+        return $this->hasMany(Player::class);
+    }
+
+    public function game()
+    {
+        return $this->hasMany(Game::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+}
