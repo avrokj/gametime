e395e77bd6353cd6e90ba65e3bd42a2cb03d7291
avrokj <jan.korva@gmail.com>
2024-05-24 10:45:30 +0300

    link fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,14 +8,14 @@
     <div class="text-center mt-4">
         <div class="flex items-center justify-center">
             <div class="w-max">
-                <h1 class="text-4xl text-amber-500 font-bold animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-amber-500 pr-5">Tere tulemast GAMETIME testimise lehele </h1>
+                <h1 class="text-4xl text-amber-500 font-bold animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-amber-500 pr-5">{{ __('Welcome to GAMETIME page') }}</h1>
             </div>
         </div>
-        <a href="{{ Route('segment.index') }}" class="text-blue-500 hover:underline">7 segment scoreboard</a>
+        <a href="https://test.gametime.ee/7-segment/" target="_blank" class="text-blue-500 hover:underline">{{ __('7 segment scoreboard') }}</a>
         <span class="mx-2 text-gray-500 dark:text-gray-400">|</span>
-        <a href="" class="text-blue-500 hover:underline">5x11 scoreboard</a>
+        <a href="https://test.gametime.ee/5x7_matrix/" target="_blank" class="text-blue-500 hover:underline">{{ __('5x11 scoreboard') }}</a>
         <span class="mx-2 text-gray-500 dark:text-gray-400">|</span>
-        <a href="" class="text-blue-500 hover:underline">memory game</a>
+        <a href="https://test.gametime.ee/memory/" target="_blank" class="text-blue-500 hover:underline">{{ __('memory game') }}</a>
     </div>
 
     <script type="module" src="{{ asset('js/app.home.js') }}"></script>
